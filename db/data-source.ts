bcf3e9cb0884160e9851bcf9402ed25d7de7a950
quangdung861 <quangdung861@gmail.com>
2026-01-09 15:47:44 +0700

    chore(global): restructuring the way environments are used

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,40 +1,17 @@
-import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
 
-const isLocal = process.env.NODE_ENV !== 'production'; // hoặc check biến khác nếu dùng custom
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV || 'development'}`,
+});
 
-const localConfig: DataSourceOptions = {
+export default new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST_DEV,
-  port: +process.env.DB_PORT_DEV,
-  username: process.env.DB_USERNAME_DEV,
-  password: process.env.DB_PASSWORD_DEV,
-  database: process.env.DB_NAME_DEV,
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js'],
-  migrationsTableName: 'custom_migration_table',
-  synchronize: false,
-};
-
-const cloudConfig: DataSourceOptions = {
-  type: 'mysql',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  ssl: {
-    rejectUnauthorized: true, // bắt buộc với cloud
-  },
-  entities: ['dist/**/*.entity.js'],
-  migrations: ['dist/db/migrations/*.js'],
-  migrationsTableName: 'custom_migration_table',
-  synchronize: true,
-};
-
-// 👇 export riêng config
-export const dataSourceOptions = isLocal ? localConfig : cloudConfig;
-
-// 👇 instance cho CLI hoặc nơi khác nếu cần
-const dataSource = new DataSource(dataSourceOptions);
-
-export default dataSource;
+});
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-import { dataSourceOptions } from 'db/data-source';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoryModule } from './category/category.module';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { typeOrmConfig } from 'config/typeorm.config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(dataSourceOptions),
     ConfigModule.forRoot({
-      isGlobal: true, // Đặt module này thành global để không cần import lại trong các module khác
+      isGlobal: true, // Set this module to global so you don't need to import it again in other modules.
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: typeOrmConfig,
     }),
     CategoryModule,
     UserModule,
@@ -21,4 +25,4 @@ import { ProductModule } from './product/product.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
